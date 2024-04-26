8065e7c01a90ed03712b2da564198e09b5ed1359
Ejdeguzman72 <ejdeguzman72@gmail.com>
2024-04-25 21:33:16 -0400

    more changes

--- App.tsx
+++ App.tsx
@@ -7,9 +7,7 @@ import * as AWS from 'aws-sdk'
 import Config from 'react-native-config';
 
 AWS.config.update({
-  accessKeyId: 'AWS_ACCESS_KEY',
-  secretAccessKey: 'AWS_SECRET_KEY',
-  region: 'AWS_REGION'
+ 
 })
 
 const s3 = new AWS.S3();
